345884a2d8f41d8eb5d9b65434661df2bf6b0733
Bruno <bruno.umbelino0@gmail.com>
2021-06-03 23:02:10 -0300

    adding user validation with @hapi/joi

---
+++ src/validations.js
@@ -0,0 +1,12 @@
+const Joi = require("@hapi/joi");
+
+const loginValidation = (data) => {
+  const schema = Joi.object({
+    email: Joi.string().email().required(),
+    password: Joi.string().min(8).max(18).required(),
+  });
+
+  return schema.validate(data);
+};
+
+module.exports = { loginValidation };
