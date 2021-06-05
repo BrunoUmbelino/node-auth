eb037ed196bede63ee07b0c5b3f6ce989cd45be7
Bruno <bruno.umbelino0@gmail.com>
2021-06-05 12:12:50 -0300

    improving readability of error logs

--- src/service/authLocalUser.js
+++ src/service/authLocalUser.js
@@ -5,12 +5,15 @@ const User = require("../model/User");
 const authLocalUser = async (email, password) => {
   try {
     const userLocal = await User.findOne({ email: email });
-    if (!userLocal) throw new Error("usuário");
+    if (!userLocal) throw new Error(`"User not found in database.`);
 
     const validPass = await bcrypt.compare(password, userLocal.password);
-    if (!validPass) throw new Error("usuário");
+    if (!validPass) throw new Error(`Password does not match.`);
 
-    const token = jwt.sign({ _id: userLocal._id }, process.env.TOKEN_SECRET);
+    const token = await jwt.sign(
+      { id: userLocal._id },
+      process.env.TOKEN_SECRET
+    );
     return { token: token, username: userLocal.email };
   } catch (error) {
     console.log(error);
